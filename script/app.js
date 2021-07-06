8f2fc78895d329669b92bc9255e9abe44449c361
Menna Rashad <mennarashad301@gmail.com>
2021-07-06 02:18:33 +0200

    fixed a tags

---
+++ script/app.js
@@ -0,0 +1,12 @@
+/*hover effect on social icons*/
+const facebook = document.getElementById("facebook");
+const twitter = document.getElementById("twitter");
+const instagram = document.getElementById("instagram");
+const pinterest = document.getElementById("pinterest");
+
+facebook.addEventListener("mouseover", (e) => {
+  facebook.firstElementChild.firstElementChild.src =
+    "..//images/white social svg/icon-facebook.svg";
+});
+/* TODO: hover effect to change img src to white svg*/
+/* TODO: Menu too T.T*/
