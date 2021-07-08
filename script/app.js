01eb673b516dd0dd469b64d71da641a10c901344
Menna Rashad <mennarashad301@gmail.com>
2021-07-08 18:40:15 +0200

    fixed menu issue on desktop view, finished desktop view

--- script/app.js
+++ script/app.js
@@ -1,16 +1,4 @@
-/*hover effect on social icons*/
-const facebook = document.getElementById("facebook");
-const twitter = document.getElementById("twitter");
-const instagram = document.getElementById("instagram");
-const pinterest = document.getElementById("pinterest");
-
-facebook.addEventListener("mouseover", (e) => {
-  facebook.firstElementChild.firstElementChild.src =
-    "..//images/white social svg/icon-facebook.svg";
-});
 /* TODO: hover effect to change img src to white svg*/
-/* TODO: menu displayed none at desktop view*/
-
 /*menu button*/
 
 const menu = document.querySelector(".toggle-button");
